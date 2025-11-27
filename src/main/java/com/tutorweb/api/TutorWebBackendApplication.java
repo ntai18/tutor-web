46a68137c617c98ba8937578964aa27461ee7c81
Tai <nguyentai18062005@gmail.com>
2025-11-27 12:11:50 +0700

    feature: add template code

---
+++ src/main/java/com/tutorweb/api/TutorWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tutorweb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorWebBackendApplication.class, args);
+    }
+
+}
