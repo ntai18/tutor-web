eae9f6ee2698501eb96927fc1ff294ee9c6278dc
Tai <nguyentai18062005@gmail.com>
2026-01-31 05:19:10 +0700

    feature: list class me, delete class me

--- src/main/java/com/tutorweb/api/service/ClassService.java
+++ src/main/java/com/tutorweb/api/service/ClassService.java
@@ -3,7 +3,11 @@ package com.tutorweb.api.service;
 import com.tutorweb.api.model.dto.request.ClassRequest;
 import com.tutorweb.api.model.dto.response.ClassResponse;
 
+import java.util.List;
+
 public interface ClassService {
     ClassResponse createClass(ClassRequest classRequest);
     ClassResponse editClass(Long classId, ClassRequest classRequest);
+    void deleteClassMe(Long classId);
+    List<ClassResponse> getClassMe();
 }
